7d8737af4dd2381443c659810474a8fc6e6bfe42
Tiago-0liveira <tfgoliveira@gmail.com>
2023-01-22 02:00:01 +0000

    newRowValidator

---
+++ electron/helpers.ts
@@ -0,0 +1,17 @@
+import { ValidateError } from "./consts"
+
+export const validateNewRow = (data: NewRowData): ValidateError | NewRowData => {
+	const { site, email, password, username } = data
+
+	if (site.length <= 3) {
+		return ValidateError.site
+	} else if (email.length <= 3) {
+		return ValidateError.emailLength
+	} else if (!/(.+)@(.+){2,}\.(.+){2,}/) {
+		return ValidateError.emailFormat
+	} else if (password.length <= 3) {
+		return ValidateError.password
+	} else {
+		return data
+	}
+}
