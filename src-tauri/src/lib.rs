6bf99a95b9ecc0962d4cd01b3aa6a6b89d24b434
Tiago-0liveira <tfgoliveira@gmail.com>
2025-09-23 23:29:23 +0100

    moved init logic to a thread so it takes a little bit of the shoulders on app startup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -26,27 +26,37 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .setup(|app| {
-            if cfg!(debug_assertions) {
-                match DB_PATH.set("test.db") {
-                    Ok(()) => return Ok(()),
-                    Err(err) => panic!("Failed to set DB_PATH: {:?}", err),
-                }
-            };
-            let handle = app.handle();
-
-            let app_data_path = match handle.path().app_local_data_dir() {
-                Ok(path) => path.join("db.db").to_str().unwrap().to_string(),
-                Err(err) => panic!("Failed to get app data path: {:?}", err),
-            };
-            eprintln!("App data path: {}", app_data_path);
-            let path: &'static str = Box::leak(app_data_path.into_boxed_str());
-            match DB_PATH.set(path) {
-                Ok(()) => Ok(()),
-                Err(err) => panic!("Failed to set DB_PATH: {:?}", err),
-            }
-        })
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            // Clone a handle that is safe to move into async
+            let app_handle = app.handle().clone();
+
+            // Run DB setup in background so it doesn't block window creation
+            tauri::async_runtime::spawn(async move {
+                if cfg!(debug_assertions) {
+                    if let Err(err) = DB_PATH.set("test.db") {
+                        eprintln!("Failed to set DB_PATH: {:?}", err);
+                    }
+                } else {
+                    match app_handle.path().app_local_data_dir() {
+                        Ok(path) => {
+                            let app_data_path = path.join("db.db");
+                            let path_str = app_data_path.to_str().unwrap().to_string();
+                            eprintln!("App data path: {}", path_str);
+                            let leaked: &'static str = Box::leak(path_str.into_boxed_str());
+                            if let Err(err) = DB_PATH.set(leaked) {
+                                eprintln!("Failed to set DB_PATH: {:?}", err);
+                            }
+                        }
+                        Err(err) => {
+                            eprintln!("Failed to get app data path: {:?}", err);
+                        }
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             /* password_vault handlers */
             password_vault_get_all_rows,
